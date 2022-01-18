1580271bdfaa20445cec5e7fafa764f6d8738ae5
Dong.Hao <donghao19910704@qq.com>
2022-01-18 15:20:52 +0800

    Oauth2：授权码模式和密码模式

---
+++ oauth2-server/src/main/java/com/donghao/cloud/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.donghao.cloud.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: DongHao
+ * @Date: 2022/1/18 14:32
+ * @Description:
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/getCurrentUser")
+    public Object getCurrentUser(Authentication authentication) {
+        return authentication.getPrincipal();
+    }
+}
